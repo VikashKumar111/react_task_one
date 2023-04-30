55b8cd1cece2d3357a269589ee3dec0eec09eae5
Vikash Kumar <vikasthakur9310@gmail.com>
2023-04-30 23:07:48 +0530

    add

--- src/Pages/Home/component/Features/index.jsx
+++ src/Pages/Home/component/Features/index.jsx
@@ -2,17 +2,50 @@ import React from "react";
 
 const Feature = () => {
   return (
-    <div className="container">
-      <div className="feature">
-        <div className="text">
-          <h6>Our Continued Project Assistnce Programs</h6>
+    <div className="feature">
+      <div className="text">
+        <h6>Our Continued Project Assistnce Programs</h6>
+      </div>
+      <div className="f-box">
+        <div id="one">
+          <p>
+            The warming of the atmosphere has an impact on weather patterns,
+            causing more frequent and severe droughts and storms worldwide.
+          </p>
         </div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div id="two">
+          <p>
+            Higher temperatures are shortening the seasons and, in some places,
+            have already exceeded safe levels for ecosystems and humans.
+          </p>
+        </div>
+        <div id="three">
+          <p>
+            This energy transition has the potential to impact many aspects of
+            life, including the environment, society, the economy, and
+            governance.
+          </p>
+        </div>
+        <div id="four">
+          <p>
+            Sustainable aquaculture, e.g. grouper aquaculture, meets the high
+            demand for reef fish while potentially preserving coastal
+            communities' livelihoods.
+          </p>
+        </div>
+        <div id="five">
+          <p>
+            Solubility is differentiated by whether the substance is easily
+            dissolved in organic solvents under normal environmental conditions
+            of temperature & pressure.
+          </p>
+        </div>
+        <div id="six">
+          <p>
+            Radioactive materials released into the environment can contaminate
+            the air, water, surfaces, soil, plants, buildings, people & animals.
+          </p>
+        </div>
       </div>
     </div>
   );
